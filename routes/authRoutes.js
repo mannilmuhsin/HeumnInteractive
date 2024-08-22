d27f0de5127e9c92afe60f326d89142175274227
mannilmuhsin <mannilmuhsin3@gmail.com>
2024-08-22 11:50:17 +0530

    handling error

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,6 +2,13 @@ const express = require('express');
 const { register, login } = require('../controllers/authController');
 const router = express.Router();
 
+router.get('/', (req,res)=>{
+    try {
+        res.send("success")
+    } catch (error) {
+        console.log(error.message);
+    }
+});
 router.post('/register', register);
 router.post('/login', login);
 
